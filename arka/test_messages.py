ac24d457f47cfb291d2526b90d97ef9eba9db056
trstovall <trstovall@users.noreply.github.com>
2024-12-27 18:41:21 -0600

    Create test_messages.py

---
+++ arka/test_messages.py
@@ -0,0 +1,17 @@
+
+import messages
+
+def test_auto():
+    #
+    print("messages.SocketMessageEnum")
+    for i in dir(messages.SocketMessageEnum):
+        if not i.startswith('__'):
+            print((i, getattr(messages.SocketMessageEnum, i).value))
+    print()
+
+
+if __name__ == "__main__":
+    #
+    print("test_auto()")
+    test_auto()
+

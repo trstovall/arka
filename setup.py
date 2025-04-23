db0278f9eb9eb94dd4a1ed0a1dbcaa84ac29fe81
T. R. Stovall <trstovall@gmail.com>
2025-04-23 09:41:21 -0500

    List details in pyproject.toml

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ _crypto = Extension(
 setup(
     name="arka",
     version="0.0.1",
-    description="short description",
-    long_description="long description",
     packages=find_packages(where="."),
     python_requires=">=3.10, <4",
     ext_modules=[_crypto]

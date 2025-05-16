9d3968157a4057f227aac14243983f5729166009
Thomas Ryan Stovall <trstovall@gmail.com>
2025-05-16 14:05:18 -0500

    Reduce packet drop rate to 10% for deploy testing

--- tests/test_net.py
+++ tests/test_net.py
@@ -328,7 +328,7 @@ async def test_dropped_packets(socket_pair: tuple[net.Socket, net.Socket]):
     A, B = socket_pair
     A_connected, B_connected, A_closed, B_closed = build_futures(socket_pair)
     A.transport._debug = True
-    A.transport._drop = 0.5         # 10% dropped packets
+    A.transport._drop = 0.1         # 10% dropped packets
     A.transport._latency = .05      # 50 ms
     # Connect
     A.connect()

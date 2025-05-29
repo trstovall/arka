52ddef04fd747edc62982e1ddb87705bf3e2e51f
Thomas Ryan Stovall <trstovall@gmail.com>
2025-05-28 19:35:40 -0500

    Sketch chain.Chain

--- arka/chain.py
+++ arka/chain.py
@@ -1,56 +1,55 @@
 
-import sqlite3
 from pathlib import Path
+from arka import block
+from arka import broker
 
-from .tx import WorkBlock, PaymentBlock
+
+class AbstractDeltaItem(object):
+    pass
+
 
 class Chain(object):
 
     forks = {}
 
-    def __init__(self, 
-        path: str | Path = '~/.arka/chain.db'
+    def __init__(self,
+        broker: broker.Broker,
+        path: str | Path = '~/.arka/',
+        mode: int = 0o700
     ):
-        self.path: Path = (path if isinstance(path, Path) else Path(path)).expanduser()
-        self.path.parent.mkdir(parents=True, exist_ok=True)
-        self.db = sqlite3.connect(str(self.path))
-        self.cur = self.db.cursor()
-        self.cur.execute("""
-            CREATE TABLE IF NOT EXISTS
-            blocks (
-                id          INTEGER,
-                digest      BLOB,
-                data        BLOB
-            );
-        """)
-        self.cur.execute("""
-            CREATE TABLE IF NOT EXISTS
-            outputs (
-                id          BLOB,
-                address     BLOB,
-                units       BLOB,
-                block_id    INTEGER,
-                spent_id    INTEGER
-            );
-        """)
-        self.cur.execute("""
-            CREATE TABLE IF NOT EXISTS
-            workstamps (
-                id          BLOB,
-                block_id    INTEGER
-            );
-        """)
+        self.broker = broker
+        self.path: Path = (
+            path if isinstance(path, Path) else Path(path)
+        ).expanduser()
+        self.path.mkdir(mode=mode, parents=True, exist_ok=True)
 
-    @property
-    def last_block(self):
+    async def accept(self, block: block.Block) -> bool:
         pass
 
-    def prepare_block(self, worker: bytes | None = None) -> WorkBlock:
+    async def expire(self, block: int) -> bool:
         pass
 
-    def add_block(self, block: Block) -> bool:
+    async def validate(self, tx: block.Transaction) -> list[AbstractDeltaItem]:
         pass
 
+    async def parameters(self, block: int | None = None) -> block.Parameters:
+        pass
 
-class PaymentPool(object):
-    pass
+    async def checkpoint(self, block: int) -> bool:
+        pass
+
+    async def fork(self, block: int) -> block.Block:
+        pass
+
+    async def hash(self) -> block.BlockHash:
+        pass
+
+    async def utxo(self,
+        ref: block.UTXORefByIndex | block.UTXORefByHash
+    ) -> block.UTXOSpawn | None:
+        pass
+
+    async def signer(self,
+        ref: block.UTXORefByIndex | block.UTXORefByHash
+    ) -> block.SignerKey | None:
+        pass
